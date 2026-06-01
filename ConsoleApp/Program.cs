7e959a0774ff5ccfedc3f68f6d261b6c3308a4a8
Chris Dowling <chris@yjcsolutions.co.uk>
2026-06-01 11:29:30 +0100

    Awaited .Stop correctly

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,21 +17,19 @@ namespace ConsoleApp;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         Trace.Listeners.Add(new ConsoleTraceListener());
 
-
         while (true)
         {
-            // DiagnosticHostingService.Start("http://localhost:2803/diagnostics");
-            // DiagnosticHostingService.Start("http://localhost:5000/diagnostics");
-            DiagnosticHostingService.Start("http://localhost:2804/diagnostics, http://localhost:6002/diagnostics");
-            // DiagnosticHostingService.Start("https://localhost:7097/diagnostics");
-            // DiagnosticHostingService.Start("http://localhost/api/diagnostics");
+            DiagnosticHostingService.Start("http://localhost:2804/diagnostics");
+
             Console.WriteLine("Diagnostics started");
             Console.ReadLine();
-            DiagnosticHostingService.Stop();
+
+            await DiagnosticHostingService.Stop();
+
             Console.WriteLine("Diagnostics stopped");
             Console.ReadLine();
         }
