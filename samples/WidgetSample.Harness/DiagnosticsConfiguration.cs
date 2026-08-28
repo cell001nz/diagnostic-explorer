264200deec0a36a3b53b40a2ec3c24211e1579e2
cell001nz <cameronelliot@gmail.com>
2026-08-28 13:27:48 +1200

    Test harness fix

--- samples/WidgetSample.Harness/DiagnosticsConfiguration.cs
+++ samples/WidgetSample.Harness/DiagnosticsConfiguration.cs
@@ -50,7 +50,7 @@ internal static class DiagnosticsConfiguration
             .UseMatchMode(EventSinkRouteMatchMode.AllMatches)
             .Route(typeof(Widget).FullName, route => route.AtLeast(LogLevel.Information).To(RouteValue.LoggerSuffix, "Widget Events2"))
             .Route(typeof(Gadget).FullName, route => route.AtLeast(LogLevel.Information).To("Form 1", "Gadget Events"))
-            .Route("WidgetSample.Form1", route => route.AtLeast(LogLevel.Trace).To("Form 1", "Form1 Events Only"))
+            .Route(typeof(Form1).FullName, route => route.AtLeast(LogLevel.Trace).To("Form 1", "Form1 Events Only"))
             .Route("*", route => route.To("System", "Events"));
     }
 
